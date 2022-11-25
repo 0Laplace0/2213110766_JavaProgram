28a167367a3b472e6081c2c8b13098996602769b
0Laplace0 <Kritpanit3162@gmail.com>
2022-11-25 12:10:26 +0700

    Java Week03

--- Week03/src/SalespersonSalary.java
+++ Week03/src/SalespersonSalary.java
@@ -1,25 +1,25 @@
-import javax.swing.*;
-import java.util.Scanner;
 import java.text.*;
+import java.util.Scanner;
 
 public class SalespersonSalary {
 	static final double COMMISSION_RATE = 0.15;
 	static final int SENTINEL = -1;
-
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		DecimalFormat frm = new DecimalFormat("#,##0.00");
 		int sales;
-		double salary = 0;
+		double salary;
 		do {
-		System.out.print("Enter sales in dollors (or -1 to end): ");
-		sales = scan.nextInt();
-		if (sales!=-1) {
-		salary = 1000 + sales * COMMISSION_RATE;
-		System.out.println("Salary is: $" + frm.format(salary));
-		}
-		else System.out.println("bye\n");
-		}while(sales!=-1);
+			System.out.print("Enter sales in dollors (or -1 to end): ");
+			sales = scan.nextInt();
+			if (sales != SENTINEL) {
+				salary = 1000 + sales * COMMISSION_RATE;
+				System.out.println("Salary i: $" + frm.format(salary));
+			}
+			else System.out.println("bye");
+		}while (sales != SENTINEL);
+		
 	}
 
 }
